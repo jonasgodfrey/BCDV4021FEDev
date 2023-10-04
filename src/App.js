de42580964dcd78787440841b24e83ea75b68723
jonasgodfrey <godfreyjtech2020@gmail.com>
2023-10-04 00:39:49 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Greeter from './Greet' ; //import the Greeter component
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React</h1>
+      <Greeter />
+    </div>
+  );
+}
+
+export default App;
